5d6d5b599b381b453b6314b717725054bafe08f7
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-03 01:39:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Write your solution in this file!
 const employee = {
-    name : "Earnest",
-    streetAddress : 52
-}
+                    name : "Earnest",
+                    streetAddress : 52
+                }
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
-     
+     const employee2 = {...employee};
+     employee2[key] = value;
+     return employee2;
+
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-
+     employee[key] = value;
+     return employee;
 }
 
 function deleteFromEmployeeByKey(emoloyee, key){
