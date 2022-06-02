9f75389e159b820c7ccb1383d582bbe5d7986efc
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-03 00:00:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+    name : "Earnest",
+    streetAddress : 52
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+}
+
+function deleteFromEmployeeByKey(emoloyee, key){
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+}
